e448d24a5ab7033e6e4b9928b5019a44e9990fba
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-06-16 13:43:45 -0500

    CSC 2018/06/16: Uso de props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-            <Navigation />
+            <Navigation titulo="Mi primera navegación"/>
           <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,9 @@ export default class Navigation extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark">
-          <a href="" className="text-white">Tasks</a>
+          <a href="" className="text-white">
+            { this.props.titulo }
+          </a>
       </nav>
     )
   }
