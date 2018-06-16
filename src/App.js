cc3da31a135baf0578f67079b7717f48519cca7c
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-06-16 12:52:27 -0500

    CSC 2018/06/16: Entendiendo react

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+          <nav className="navbar navbar-dark bg-dark">
+             <a href="" className="text-white">Tasks</a>
+          </nav>
+          
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
