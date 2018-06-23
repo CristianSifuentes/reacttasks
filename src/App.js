7b579b8f31295e47524521c71d0cad711fc9daae
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-06-23 16:20:38 -0500

    CSC 2018/06/23: Aprendiendo a usar el estado de la aplicación

--- src/App.js
+++ src/App.js
@@ -3,13 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation';
 
+import { todos } from './components/todos.json';
+console.log(todos);
+
 class App extends Component {
+
+ constructor(){
+   //necesario super
+   super();
+   this.state = {
+      todos
+   }
+ }
+
+
   render() {
+
+
+ const todos = this.state.todos.map((todo, i) => {
+      return (
+          <div className="card">
+             { todo.title  }
+          </div>
+      )
+  });
+
     return (
       <div className="App">
-            <Navigation titulo="Mi primera navegación"/>
-            <Navigation titulo="Mi segunda navegación"/>
-
+          <nav className="navbar navbar-dark bg-dark">
+              <a href="" className="text-white">
+             
+              </a> 
+          </nav>
+            {  todos }
           <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
